c8d212880a35b5b599656421a74ef70f590f65f1
VytraDev <vytradev@gmail.com>
2026-09-14 13:41:52 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Build-a-Smart-Home-Security-Dashboard/", // <-- add this
+  base: "https://kaychiawoon-deploy-help.vercel.app", // <-- add this
   plugins: [react()],
 });
